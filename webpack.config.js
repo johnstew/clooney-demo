338faa1e3a26e1808432a3b2d75016c8a01bf205
John Stewart <me@johnstewart.io>
2018-09-11 11:42:38 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index.bundle.js',
-    globalObject: 'window'
+    globalObject: 'this'
   },
   plugins: [new HtmlWebpackPlugin()]
 };
